58db82fc88bb802197446698baa921d553be636f
Tuan Chau <chau.vinh.tuan@linecorp.com>
2022-11-12 07:34:43 +0900

    Format

--- apng/src/main/java/com/github/penfeizhou/animation/apng/io/APNGReader.kt
+++ apng/src/main/java/com/github/penfeizhou/animation/apng/io/APNGReader.kt
@@ -9,24 +9,18 @@ object APNGReader {
     fun FilterReader.readInt(): Int {
         val buf = ensureBytes()
         read(buf, 0, 4)
-        return buf[3].toInt() and 0xFF or (
-            buf[2].toInt() and 0xFF shl 8
-            ) or (
-            buf[1].toInt() and 0xFF shl 16
-            ) or (
-            buf[0].toInt() and 0xFF shl 24
-            )
+        return (buf[3].toInt() and 0xFF) or
+                (buf[2].toInt() and 0xFF shl 8) or
+                (buf[1].toInt() and 0xFF shl 16) or
+                (buf[0].toInt() and 0xFF shl 24)
     }
 
     @Throws(IOException::class)
     fun FilterReader.readShort(): Short {
         val buf = ensureBytes()
         read(buf, 0, 2)
-        return (
-            buf[1].toInt() and 0xFF or (
-                buf[0].toInt() and 0xFF shl 8
-                )
-            ).toShort()
+        return (buf[1].toInt() and 0xFF or
+                (buf[0].toInt() and 0xFF shl 8)).toShort()
     }
 
     /**
@@ -50,7 +44,10 @@ object APNGReader {
     fun FilterReader.readFourCC(): Int {
         val buf = ensureBytes()
         read(buf, 0, 4)
-        return buf[0].toInt() and 0xff or (buf[1].toInt() and 0xff shl 8) or (buf[2].toInt() and 0xff shl 16) or (buf[3].toInt() and 0xff shl 24)
+        return (buf[0].toInt() and 0xff) or
+                (buf[1].toInt() and 0xff shl 8) or
+                (buf[2].toInt() and 0xff shl 16) or
+                (buf[3].toInt() and 0xff shl 24)
     }
 
     private val THREAD_LOCAL_BYTE_BUFFERS = ThreadLocal<ByteArray>()
