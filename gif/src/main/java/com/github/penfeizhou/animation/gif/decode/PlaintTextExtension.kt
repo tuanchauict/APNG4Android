fa5238cdb747a886b2598e1c77354831ea5c266d
Tuan Chau <chau.vinh.tuan@linecorp.com>
2022-11-12 06:58:10 +0900

    Kotlinize PlaintTextExtension

---
+++ gif/src/main/java/com/github/penfeizhou/animation/gif/decode/PlaintTextExtension.kt
@@ -0,0 +1,31 @@
+package com.github.penfeizhou.animation.gif.decode
+
+import com.github.penfeizhou.animation.gif.io.GifReader
+import java.io.IOException
+
+/**
+ * @Description: APNG4Android
+ * @Author: pengfei.zhou
+ * @CreateDate: 2019-05-17
+ */
+class PlaintTextExtension : ExtensionBlock() {
+    private val plainTextData: MutableList<DataSubBlock> = ArrayList()
+    @Throws(IOException::class)
+    override fun receive(reader: GifReader) {
+        val blockSize = reader.peek().toInt()
+        val x = reader.readUInt16()
+        val y = reader.readUInt16()
+        val width = reader.readUInt16()
+        val height = reader.readUInt16()
+        val characterCellWidth = reader.peek().toInt()
+        val characterCellHeight = reader.peek().toInt()
+        val fgColorIndex = reader.peek().toInt()
+        val bgColorIndex = reader.peek().toInt()
+        var dataSubBlock: DataSubBlock
+        while (!DataSubBlock.retrieve(reader).also { dataSubBlock = it }.isTerminal) {
+            plainTextData.add(dataSubBlock)
+        }
+    }
+
+    override fun size(): Int = 0
+}
