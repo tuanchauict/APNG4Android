37b0ad8112811a066cf19f15369c4b0787747cdd
Tuan Chau <chau.vinh.tuan@linecorp.com>
2022-11-20 19:13:46 +0900

    Replace parent of StillFrame to KFrame

---
+++ awebp/src/main/java/com/github/penfeizhou/animation/webp/decode/StillFrame.kt
@@ -0,0 +1,61 @@
+package com.github.penfeizhou.animation.webp.decode
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Paint
+import com.github.penfeizhou.animation.decode.KFrame
+import com.github.penfeizhou.animation.io.FilterReader
+import com.github.penfeizhou.animation.io.Writer
+import java.io.IOException
+import java.lang.IllegalArgumentException
+
+/**
+ * @Description: StillFrame
+ * @Author: pengfei.zhou
+ * @CreateDate: 2019-05-13
+ */
+class StillFrame(
+    private val reader: FilterReader,
+    width: Int,
+    height: Int
+) : KFrame(x = 0, y = 0, width = width, height = height, duration = 0) {
+    init {
+        frameWidth = width
+        frameHeight = height
+    }
+
+    override fun draw(
+        canvas: Canvas,
+        paint: Paint,
+        sampleSize: Int,
+        reusedBitmap: Bitmap,
+        writer: Writer
+    ): Bitmap? {
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = false
+        options.inSampleSize = sampleSize
+        options.inMutable = true
+        options.inBitmap = reusedBitmap
+        var bitmap: Bitmap? = null
+        try {
+            try {
+                bitmap = BitmapFactory.decodeStream(reader.toInputStream(), null, options)
+            } catch (e: IllegalArgumentException) {
+                e.printStackTrace()
+                // Problem decoding into existing bitmap when on Android 4.2.2 & 4.3
+                val optionsFixed = BitmapFactory.Options()
+                optionsFixed.inJustDecodeBounds = false
+                optionsFixed.inSampleSize = sampleSize
+                optionsFixed.inMutable = true
+                bitmap = BitmapFactory.decodeStream(reader.toInputStream(), null, optionsFixed)
+            }
+            assert(bitmap != null)
+            paint.xfermode = null
+            canvas.drawBitmap(bitmap!!, 0f, 0f, paint)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return bitmap
+    }
+}
